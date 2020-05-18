7330c167e7145a045f7dfdf853ccf85e01ce252d
Philippe Caparroy <philippecaparroy@orange.fr>
2020-05-18 16:46:31 +0200

    ok

---
+++ src/main/java/fr/dga/Test.java
@@ -0,0 +1,25 @@
+package fr.dga;
+
+/**
+ * Hello world!
+ *
+ */
+public class Test 
+{
+	
+	
+	
+	public static void readAvro(){
+	
+		
+	}	
+	
+	
+    public static void main( String[] args )
+    {
+ 
+        System.out.println( "Hello World!" );
+       
+        readAvro();
+    }
+}
