b505eb8ef2bccc9583043cd07ec9f6817fec1dd1
QuentinLEONE <foo@bar.com>
2020-05-19 10:47:51 +0200

    add frequency

--- src/main/java/fr/dga/Test.java
+++ src/main/java/fr/dga/Test.java
@@ -2,24 +2,28 @@ package fr.dga;
 
 /**
  * Hello world!
- *
  */
-public class Test 
-{
-	
-	
-	
-	public static void readAvro(){
-	
-		
-	}	
-	
-	
-    public static void main( String[] args )
-    {
- 
-        System.out.println( "Hello World!" );
-       
+
+public class Test {
+
+
+    public static void readAvro() {
+
+
+    }
+
+    public static void frequency(String s) {
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
         readAvro();
+
+        frequency("");
     }
 }
